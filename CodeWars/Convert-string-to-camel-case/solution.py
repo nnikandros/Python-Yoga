0c25ba98d2f0ca4ecf310e2f3d15d7c2c2c47891
Nikitas Nikandros <58775214+nnikandros@users.noreply.github.com>
2022-07-16 19:03:38 +0200

    Update solution.py

--- CodeWars/Convert-string-to-camel-case/solution.py
+++ CodeWars/Convert-string-to-camel-case/solution.py
@@ -9,3 +9,11 @@ def to_camel_case(text):
     for i,j in enumerate(textlist):
         sum+=j
     return sum    
+
+
+######
+def to_camel_case(text):
+    removed = text.replace('-', ' ').replace('_', ' ').split()
+    if len(removed) == 0:
+        return ''
+    return removed[0]+ ''.join([x.capitalize() for x in removed[1:]])
