c7e2b4566a9f9e5f4929b17cea99d269ab2cf79f
Nikitas Nikandros <58775214+nnikandros@users.noreply.github.com>
2022-07-21 19:55:49 +0200

    Create solution.py

---
+++ CodeWars/Replace-With-Alphabet-Position/solution.py
@@ -0,0 +1,10 @@
+def alphabet_position(text):
+    #make a dictionary of letters and numbers
+    letters=[chr(i) for i in range(ord('a'),ord('z')+1)]
+    numbers=[i for i in range(27) if i>=1]
+    new_dict = dict(zip(letters, numbers))
+    #change into lower case
+    text=text.lower()
+    lst=list(text)
+    p=[str(new_dict[value]) for value in lst if value.isalpha()   ]
+    return " ".join(p)
