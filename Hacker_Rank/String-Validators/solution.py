cb76cd5385b4f345835a30d76bc30faa4934666d
Nikitas Nikandros <58775214+nnikandros@users.noreply.github.com>
2022-08-01 18:56:04 +0200

    Delete Hacker_Rank/String-Validators directory

--- Hacker_Rank/String-Validators/solution.py
+++
@@ -1,5 +0,0 @@
-print(True) if  any([x.isalnum() for x in s  ]) else print(False)
-print(True) if  any([x.isalpha() for x in s  ]) else print(False)
-print(True) if  any([x.isdigit() for x in s  ]) else print(False)
-print(True) if  any([x.islower() for x in s  ]) else print(False)
-print(True) if  any([x.isupper() for x in s  ]) else print(False)
