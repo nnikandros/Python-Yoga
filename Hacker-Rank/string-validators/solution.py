d955e32970ab71a479191c036334760c8e73008c
Nikitas Nikandros <58775214+nnikandros@users.noreply.github.com>
2022-08-01 23:28:36 +0200

    Create solution.py

---
+++ Hacker-Rank/string-validators/solution.py
@@ -0,0 +1,5 @@
+print(True) if  any([x.isalnum() for x in s  ]) else print(False)
+print(True) if  any([x.isalpha() for x in s  ]) else print(False)
+print(True) if  any([x.isdigit() for x in s  ]) else print(False)
+print(True) if  any([x.islower() for x in s  ]) else print(False)
+print(True) if  any([x.isupper() for x in s  ]) else print(False)]
