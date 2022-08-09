3503b3e4d0ebd84f0631e129e75d5cf6c743098b
Nikitas Nikandros <58775214+nnikandros@users.noreply.github.com>
2022-08-09 17:28:50 +0200

    Update solution.py

--- Project_Euler/Problem-25/solution.py
+++ Project_Euler/Problem-25/solution.py
@@ -43,4 +43,14 @@ def fibboclosed(n):
 
 ## just need to fix an overflow error :]   
   
+   ## another solution from the forum 
+   n = 2
+prev = 1
+curr = 1
+while curr < 10 ** 999:
+    prev, curr = curr, prev + curr
+    n += 1
+
+print(f"n = {n}")
+print(f"Fibonacci number = {curr}")
   
